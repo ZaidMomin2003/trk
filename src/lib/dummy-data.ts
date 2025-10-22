aa12436d60bd449285f3384038f3cb78c6077799
Zaid Momin <codesensei321@gmail.com>
2025-10-22 02:39:06 +0000

    remove this. i dont want this (_for element li_)

--- src/lib/dummy-data.ts
+++ src/lib/dummy-data.ts
@@ -14,12 +14,11 @@ export const orders: Order[] = [
     id: 'Arshad-LLM-2025-2568634512',
     trackingNumber: 'Zaidaws@123456',
     productName: 'AWS SWAG Builder Kit',
-    productImage: 'aws.jpeg',
+    productImage: '/aws.jpeg',
     estimatedDelivery: 'October 26, 2025',
     status: 'Out for Delivery',
     shippedTo: 'Arshad, Muddebihal, Bijapur, Karnataka',
     trackingHistory: [
-      { date: 'October 23, 2025', time: '8:00 AM', status: 'Out for delivery', location: 'Hubli, Karnataka' },
       { date: 'October 22, 2025', time: '7:15 AM', status: 'Out for Bangalore facility', location: 'Delhi, India' },
       { date: 'October 21, 2025', time: '6:26 AM', status: 'Software and Malware installation', location: 'Delhi, India' },
       { date: 'October 17, 2025', time: '04:38 AM', status: 'Product checking', location: 'Delhi, India' },
